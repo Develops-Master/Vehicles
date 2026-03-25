538b4c0d4ac814c9994803797657be9de0884670
Develops-Master <developsmaster@gmail.com>
2026-03-25 18:45:25 -0500

    VehicleType

---
+++ Vehicles.API/Data/Entities/VehicleType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vehicles.API.Data.Entities
+{
+    public class VehicleType
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El campo {0} es obligatorio")]
+        [StringLength(50, ErrorMessage = "El campo {0} debe tener entre {2} y {1} caracteres", MinimumLength = 3)]
+        [Display(Name = "Tipo de Vehículo")]
+        public int Description { get; set; }
+    }
+}
